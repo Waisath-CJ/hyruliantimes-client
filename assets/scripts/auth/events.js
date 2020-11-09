2c9d15119229431a9709932b4890b32285187a53
Waisath-CJ <curt.waisath.jr@outlook.com>
2020-11-09 13:52:06 -0700

    Add User Code
    Copied my authentication scripts from tic-tac-toe project into this project since handling the user stuff is going to work the same as before

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const api = require('./api')
+const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields')
+
+const onSignUp = e => {
+  e.preventDefault()
+  const form = e.target
+  const data = getFormFields(form)
+
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = e => {
+  e.preventDefault()
+  const form = e.target
+  const formData = getFormFields(form)
+
+  api.signIn(formData)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = e => {
+  e.preventDefault()
+  const form = e.target
+  const formData = getFormFields(form)
+
+  api.changePassword(formData)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = e => {
+  e.preventDefault()
+
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+const signUpSwitch = e => {
+  e.preventDefault()
+  ui.signUpSwitch()
+}
+
+const signInSwitch = e => {
+  e.preventDefault()
+  ui.signInSwitch()
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut,
+  signUpSwitch,
+  signInSwitch
+}

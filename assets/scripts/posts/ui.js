d3bd51460251a2b3615ab481878c17b9277cee99
Waisath-CJ <curt.waisath.jr@outlook.com>
2020-11-11 20:50:50 -0700

    Update getPostsSuccess Function
    Updated the getPostsSuccess function to display the posts properly as well as give the edit and delete buttons their event listeners because they wouldn't attach properly in the app.js file.

--- assets/scripts/posts/ui.js
+++ assets/scripts/posts/ui.js
@@ -5,20 +5,46 @@ const createPostSuccess = res => {
 }
 
 const createPostFailure = err => {
-
+  alert(err)
 }
 
 const getPostsSuccess = res => {
   const posts = res.posts
   let html = ''
+  const editHTML = '<button class="btn btn-warning inline edit-post">Edit</button>'
+  const deleteHTML = '<button class="btn btn-danger inline delete-post">Delete</button>'
   for (let i = posts.length - 1; i >= 0; i--) {
-    html += `<p>${posts[i].owner}<br>${posts[i].createdAt}<br>${posts[i].content}</p>`
+    html += `<div class="card"><div class="card-header inline options-parent">${posts[i].owner} - ${editHTML} ${deleteHTML}</div><div class="card-body"><p class="card-text">${posts[i].content}</p></div><div class="card-footer text-muted">${posts[i].createdAt}</div></div><br>`
   }
-  $('#response-area').html(html)
+  $('#posts-section').html(html)
+  $('.edit-post').on('click', function (e) {
+    e.preventDefault()
+
+    console.log('clicked edit post button')
+
+    // const form = e.target
+    // const postData = getFormFields(form)
+
+    // api.editPost(postData)
+    //   .then(ui.editPostSuccess)
+    //   .catch(ui.editPostFailure)
+  })
+  $('.delete-post').on('click', function (e) {
+    e.preventDefault()
+
+    console.log('clicked delete post button')
+
+    // const form = e.target
+    // const postData = getFormFields(form)
+
+    // api.deletePost(postData)
+    //   .then(ui.deletePostSuccess)
+    //   .catch(ui.deletePostFailure)
+  })
 }
 
 const getPostsFailure = err => {
-
+  alert(err)
 }
 
 module.exports = {

9930ee2bddf870f9844e2621a5349e2e970b32c2
Waisath-CJ <curt.waisath.jr@outlook.com>
2020-11-11 22:43:31 -0700

    Major Changes
    Removed edit post listener
    Added require statements
    Made it so buttons only appear on current user's posts and no one else's posts
    Added editPostSuccess and editPostFailure functions
    Made it so messages are shown to user upon action completion

--- assets/scripts/posts/ui.js
+++ assets/scripts/posts/ui.js
@@ -1,34 +1,33 @@
 'use strict'
 
+const api = require('./api')
+const store = require('../store')
+const getFormFields = require('../../../lib/get-form-fields')
+
 const createPostSuccess = res => {
   $('#create-post-form').trigger('reset')
+  $('#message').text('Created post successfully!')
 }
 
 const createPostFailure = err => {
-  alert(err)
+  $('#message').text('Creating post failed\n' + err)
 }
 
 const getPostsSuccess = res => {
   const posts = res.posts
+  const userFullName = `${store.user.firstName} ${store.user.lastName}`
   let html = ''
-  const editHTML = '<button class="btn btn-warning inline edit-post">Edit</button>'
-  const deleteHTML = '<button class="btn btn-danger inline delete-post">Delete</button>'
+  const deleteHTML = '<button class="btn btn-outline-danger inline delete-post">Delete</button>'
   for (let i = posts.length - 1; i >= 0; i--) {
-    html += `<div class="card"><div class="card-header inline options-parent">${posts[i].owner} - ${editHTML} ${deleteHTML}</div><div class="card-body"><p class="card-text">${posts[i].content}</p></div><div class="card-footer text-muted">${posts[i].createdAt}</div></div><br>`
+    if (userFullName === posts[i].owner) {
+      const editHTML = `<button type="button" class="btn btn-outline-warning" data-toggle="modal" data-target="#editPostModal" data-content="${posts[i].content}" data-id="${posts[i]._id}">Edit</button>`
+
+      html += `<div class="card"><div class="card-header inline options-parent">${posts[i].owner} - ${editHTML} ${deleteHTML}</div><div class="card-body"><p class="card-text">${posts[i].content}</p></div><div class="card-footer text-muted">${posts[i].createdAt}</div></div><br>`
+    } else {
+      html += `<div class="card"><div class="card-header inline options-parent">${posts[i].owner}</div><div class="card-body"><p class="card-text">${posts[i].content}</p></div><div class="card-footer text-muted">${posts[i].createdAt}</div></div><br>`
+    }
   }
   $('#posts-section').html(html)
-  $('.edit-post').on('click', function (e) {
-    e.preventDefault()
-
-    console.log('clicked edit post button')
-
-    // const form = e.target
-    // const postData = getFormFields(form)
-
-    // api.editPost(postData)
-    //   .then(ui.editPostSuccess)
-    //   .catch(ui.editPostFailure)
-  })
   $('.delete-post').on('click', function (e) {
     e.preventDefault()
 
@@ -38,18 +37,29 @@ const getPostsSuccess = res => {
     // const postData = getFormFields(form)
 
     // api.deletePost(postData)
-    //   .then(ui.deletePostSuccess)
-    //   .catch(ui.deletePostFailure)
+    //   .then(deletePostSuccess)
+    //   .catch(deletePostFailure)
   })
 }
 
 const getPostsFailure = err => {
-  alert(err)
+  $('#message').text('Getting posts failed\n' + err)
+}
+
+const editPostSuccess = res => {
+  $('#editPostModal').modal('toggle')
+  $('#message').text('Editing post succeeded')
+}
+
+const editPostFailure = err => {
+  $('#message').text('Editing post failed\n' + err)
 }
 
 module.exports = {
   createPostSuccess,
   createPostFailure,
   getPostsSuccess,
-  getPostsFailure
+  getPostsFailure,
+  editPostSuccess,
+  editPostFailure
 }
